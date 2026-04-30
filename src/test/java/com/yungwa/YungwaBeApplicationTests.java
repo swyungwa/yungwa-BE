a5ca31f7c2aa4a66b64d03a1691e186b147c0c27
yunjin1213 <yunjinkim25@mju.ac.kr>
2026-04-30 15:51:50 +0900

    feat: BE 초기 세팅

---
+++ src/test/java/com/yungwa/YungwaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yungwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YungwaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
