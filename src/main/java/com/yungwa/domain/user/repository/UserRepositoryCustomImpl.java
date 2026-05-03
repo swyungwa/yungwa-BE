2f98befad76803b3fd3bcbe17332aad5e6b0a763
이종혁(Lee JongHyeok) <dlwhdgur0509@gmail.com>
2026-05-04 04:32:31 +0900

    feat: card 도메인 및 QueryDSL 기반 동적 성별 필터링 및 보안 로직 구현

---
+++ src/main/java/com/yungwa/domain/user/repository/UserRepositoryCustomImpl.java
@@ -0,0 +1,35 @@
+package com.yungwa.domain.user.repository;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.yungwa.domain.user.domain.Gender;
+import com.yungwa.domain.user.domain.GenderFilter;
+import com.yungwa.domain.user.entity.QUser;
+import com.yungwa.domain.user.entity.User;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class UserRepositoryCustomImpl implements UserRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<User> findCardsByGenderFilter(Long viewerId, GenderFilter genderFilter) {
+        QUser user = QUser.user;
+
+        BooleanBuilder builder = new BooleanBuilder();
+        builder.and(user.id.ne(viewerId));
+        builder.and(user.deleted.isFalse());
+
+        if (genderFilter != GenderFilter.ALL) {
+            builder.and(user.gender.eq(Gender.valueOf(genderFilter.name())));
+        }
+
+        return queryFactory
+                .selectFrom(user)
+                .leftJoin(user.loveType).fetchJoin()
+                .where(builder)
+                .fetch();
+    }
+}
